c85737a59c29787dff0b842ed51446468a4ffff1
Aleksey Osipov <alexey11rus@gmail.com>
2020-02-19 22:47:44 +0300

    feat: typical arrays problems done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+  if ( !Array.isArray(array) || array.length < 1 ) return 0;
+  return (array.sort((a, b) => a-b))[0];
+  } 
+
 
 exports.max = function max (array) {
-  return 0;
+  if ( !Array.isArray(array) || array.length < 1 ) return 0;
+  return (array.sort((a, b) => a-b))[array.length-1];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if ( !Array.isArray(array) || array.length < 1 ) return 0;
+
+  let sum = array.reduce( (accum, current) => {
+    return accum += current;
+  }, 0);
+
+  return sum / array.length;
+  
 }
